b8fb29e707eec9ff4e4ce1b9047d75077523f881
ZhBauyrzhan <zhnbauyrzhan@gmail.com>
2023-05-05 17:15:37 +0600

    сбор фото работает через раз

--- parser/dynamic-parser.py
+++ parser/dynamic-parser.py
@@ -1,29 +1,35 @@
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.chrome.service import Service
 
 import time
 
-
-PATH = 'C:\Program Files (x86)\chromedriver.exe'
+serv = Service('C:/Program Files (x86)/chromedriver.exe')
 opts = ChromeOptions()
-opts.add_argument('— headless')
-driver = Chrome(executable_path=PATH, options=opts)
+# opts.headless = True
+driver = Chrome(service=serv, options=opts)
 driver.get('https://www.nike.com/t/free-metcon-5-mens-training-shoes-Vfsbpq/DV3949-700')
 driver.maximize_window()
 try:
-    accept = driver.find_element(By.CLASS_NAME, 'nds-btn dialog-actions-accept-btn css-60b779 ex41m6f0 btn-primary-dark  btn-md')
-    driver.find_element(By.XPATH, '//*[@id="modal-root"]/div/div/div/div/div/section/div[2]/div/button[1]').click()
-    time.sleep(30)
-    # // *[ @ id = "modal-root"] / div / div / div / div / div / section / div[2] / div / button[1]
-    # divs = []
-    # divs = driver.find_element(By.CLASS_NAME, 'd-sm-flx flx-jc-sm-fs flx-ai-sm-fe css-1mhv7vq')
-    # print(divs)
-    # for div in divs:
-    #     image_source = div.find_element(By.XPATH, '.// *[ @ id = "PDP"]/div[2]/div/div[4]/div[2]/div[1]/div/div[1]/div[1]/label/img')
-    #     print(image_source, 'was')
-except:
-    print('fail')
-# class="d-sm-flx flx-jc-sm-fs flx-ai-sm-fe css-1mhv7vq"
-# // *[ @ id = "PDP"] / div[2] / div / div[4] / div[2] / div[1] / div / div[1] / div[1] / label / img
-# time.sleep(30000)
+    photos = driver.find_element(by=By.XPATH, value="""/html/body/div[4]/div/div/div[2]/div/div[4]/div[2]/div[1]/div/div[1]""")
+    photos_containers = driver.find_element(by=By.XPATH, value="""/html/body/div[4]/div/div/div[2]/div/div[4]/div[2]/div[1]/div""")
+    big = driver.find_element(by=By.XPATH, value="""/html/body/div[4]/div/div/div[2]/div/div[4]/div[2]/div[1]/div/div[2]/div[1]""")
+
+    images = driver.find_elements(by=By.PARTIAL_LINK_TEXT, value="""https://static.nike.com/a/images/""")
+
+    for i in images:
+        print(i.get_attribute('src'))
+
+    print(photos.text, '1')
+    print('********************************************************')
+    print(photos_containers.text, '2')
+    img = photos.find_elements(by=By.TAG_NAME, value='img')
+    image_sources = []
+    for i in img:
+        image_sources.append( i.get_attribute('src') )
+    print(image_sources)
+
+
+    time.sleep(3000)
+except Exception as e:
+    print('fail', e)
--- parser/parse.py
+++ parser/parse.py
@@ -1,70 +1,48 @@
-from typing import OrderedDict, List, Any
-from fake_useragent import UserAgent
-
-import requests
-from bs4 import BeautifulSoup
 from PIL import Image
+import requests
+
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+import time
 
 
 class Parser:
-    ua = UserAgent()
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'}
-    @staticmethod
-    def write_html_to_file(soup: BeautifulSoup):
-        with open('html.txt', 'w') as file:
-            print(soup.prettify())
-            # data = soup.
-            # file.write(data)
+    """Нужно переписать получение chrome driver-а"""
+    serv = Service('C:/Program Files (x86)/chromedriver.exe')
+    opts = ChromeOptions()
+    # opts.headless = True
+    driver = Chrome(service=serv, options=opts)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'}
+
     @staticmethod
     def parse_photo(url: str) -> Image:
         with requests.get(url, headers=Parser.headers, stream=True).raw as raw_image:
             return Image.open(raw_image)
 
     @staticmethod
-    def parse_photos(soup: BeautifulSoup) -> list:
-        img = []
-        picture = soup.find_all('picture')
-        for i in picture:
-            image_code = i.find('img')
-            img.append(Parser.parse_photo(image_code['src']))
-        return img
+    def parse_photos(url: str, xpath: str) -> list[Image]:
+        Parser.driver.get(url)
+        Parser.driver.maximize_window()
 
-    @staticmethod
-    def parse_size(soup: BeautifulSoup) -> list[Any]:
-        print(soup.prettify())
-        # table = soup.find('fieldset', class_=" mt5-sm mb3-sm body-2 css-1pj6y87")
-        # divs = table.findChildren('div', recursive=True)
-        table = soup.find('div', class_='mt2-sm css-hzulvp')
-        # for i in divs:
-        #     print(i)
-        print(table)
-        sizes = []
-        return sizes
-    @staticmethod
-    def parse_shoes(url: str) -> None:
-        print(Parser.headers)
-        with requests.get(url, headers=Parser.headers) as html_file:
-            soup = BeautifulSoup(html_file.content, 'html.parser')
-            name = soup.find(id='pdp_product_title').text
-            gender = soup.find('h2', {'data-test': 'product-sub-title'}).text
-            price_with_currency = soup.find('div', class_='is--current-price').text
-            currency = price_with_currency[0]
-            price = price_with_currency[1:]
-            images = Parser.parse_photos(soup)
-            size = Parser.parse_size(soup)
-            # for i in size:
-            #     print(i)
-    @staticmethod
-    def parse_by_link(url: str) -> None:
-        with requests.get(url, headers=Parser.headers) as html_file:
-            soup = BeautifulSoup(html_file.content, 'html.parser')
-            products = soup.find_all("div", class_="product-card")
-            a = soup.find('a', class_="product-card__link-overlay").get('href')
-            print(a)
-
+        photos = Parser.driver.find_element(by=By.XPATH, value=xpath)
+        img = photos.find_elements(by=By.TAG_NAME, value='img')
+        images = []
+        for i in img:
+            image=Parser.parse_photo(i.get_attribute('src'))
+            images.append(image)
+            image.show()
+        return images
 
 p = Parser()
-# p.parse_by_link('https://www.adidas.com/us')
-# p.parse_by_link('https://www.nike.com/w/new-mens-shoes-3n82yznik1zy7ok')
-p.parse_shoes('https://www.nike.com/t/free-metcon-5-mens-training-shoes-Vfsbpq/DV3949-700')
-# p.parse_shoes('https://www.adidas.com/us/gazelle-shoes/IG0669.html')
+photo_storage_substring = {
+    'nike': 'https://static.nike.com/a/images/'
+}
+# p.parse_photos(url='https://www.nike.com/t/free-metcon-5-mens-training-shoes-Vfsbpq/DV3949-700',
+#               storage_substring=photo_storage_substring['nike'])
+xpath_photos = {
+    'nike': """/html/body/div[4]/div/div/div[2]/div/div[4]/div[2]/div[1]/div/div[1]"""
+}
+p.parse_photos(url='https://www.nike.com/t/free-metcon-5-mens-training-shoes-Vfsbpq/DV3949-700', xpath=xpath_photos['nike'])
