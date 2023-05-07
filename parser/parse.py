5cded054c548603b9ba4248b1dffc2a2a27fca4c
ZhBauyrzhan <zhnbauyrzhan@gmail.com>
2023-05-07 16:30:25 +0600

    немного лишние фотки.

--- parser/parse.py
+++ parser/parse.py
@@ -41,23 +41,34 @@ class Parser:
         with requests.get(url, headers=Parser.headers, stream=True).raw as raw_image:
             return Image.open(raw_image)
 
+    # @staticmethod
+    # def _parse_photos(driver: Chrome, xpath: str) -> list[Image]:
+    #     photos = driver.find_element(by=By.XPATH, value=xpath)
+    #     img = photos.find_elements(by=By.TAG_NAME, value='img')
+    #     images = []
+    #     for i in img:
+    #         image = Parser._parse_photo(i.get_attribute('src'))
+    #         images.append(image)
+    #         # image.show()
+    #     return images
+
     @staticmethod
     def _parse_photos(driver: Chrome, xpath: str) -> list[Image]:
-        photos = driver.find_element(by=By.XPATH, value=xpath)
-        img = photos.find_elements(by=By.TAG_NAME, value='img')
+        photos = driver.find_elements(by=By.TAG_NAME, value='img')
         images = []
-        for i in img:
-            image = Parser._parse_photo(i.get_attribute('src'))
-            images.append(image)
-            # image.show()
+        for i in photos:
+            url = i.get_attribute('src')
+            if url is not None and url.startswith('https://static.nike.com/'):
+                images.append(Parser._parse_photo(url))
         return images
-
     @staticmethod
     def _cookies(driver: Chrome, xpath: str) -> None:
-        button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
-        time.sleep(2)
-        button.click()
-
+        try:
+            button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+            time.sleep(2)
+            button.click()
+        except Exception as e:
+            print('No need to work with cookies')
     @staticmethod
     def _parse_name(driver: Chrome, xpath: str) -> str:
         name = driver.find_element(by=By.XPATH, value=xpath).text
@@ -71,6 +82,14 @@ class Parser:
         return price
 
     @staticmethod
+    def _country(driver: Chrome, xpath: str) -> None:
+        try:
+            button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
+            time.sleep(2)
+            button.click()
+        except Exception as e:
+            print('No need to work with country')
+    @staticmethod
     def parse_shoes(url: str, xpath: dict) -> None:
         opts = ChromeOptions()
         # opts.headless = True
@@ -78,9 +97,9 @@ class Parser:
         driver.get(url)
         driver.maximize_window()
         # time.sleep(3000)
+        Parser._country(driver=driver, xpath=xpath['country'])
         Parser._cookies(driver=driver, xpath=xpath['accept'])
         time.sleep(25)
-        sizes = Parser._parse_size(driver=driver, xpath=xpath['size'])
         name = Parser._parse_name(driver=driver, xpath=xpath['name'])
         price = Parser._parse_price(driver=driver, xpath=xpath['price'])
         images = Parser._parse_photos(driver=driver, xpath=xpath['photos'])
@@ -89,6 +108,9 @@ class Parser:
                 i.show()
         except Exception as e:
             print(e)
+        sizes = Parser._parse_size(driver=driver, xpath=xpath['size'])
+
+
 p = Parser()
 # xpath_photos = {
 #     'nike': """/html/body/div[4]/div/div/div[2]/div/div[4]/div[2]/div[1]/div/div[1]"""
@@ -104,15 +126,18 @@ p = Parser()
 
 xpath_nike = {
     'photos': """/html/body/div[4]/div/div/div[2]/div/div[4]/div[2]/div[1]/div/div[1]""",
-    'size': """/html/body/div[4]/div/div/div[2]/div/div[4]/div[2]/div[2]/div/div/div[3]/form/div[1]/fieldset""",
+    'size': """/html/body/div[4]/div/div/div[2]/div/div[4]/div[2]/div[2]/div/div/div[3]/form/div[1]/fieldset/div""",
     'accept': """/html/body/div[7]/div/div/div/div/div/section/div[2]/div/button[1]""",
     'decline': """/html/body/div[7]/div/div/div/div/div/section/div[2]/div/button[2]""",
     'name': """/html/body/div[4]/div/div/div[2]/div/div[4]/div[2]/div[2]/div/div/div[1]/div/div[2]/div/h1""",
     'price': """/html/body/div[4]/div/div/div[2]/div/div[4]/div[2]/div[2]/div/div/div[1]/div
     /div[2]/div/div/div/div/div""",
+    'country': """/html/body/div[6]/div/div/nav/button"""
 }
 
-p.parse_shoes(url='https://www.nike.com/t/free-metcon-5-mens-training-shoes-Vfsbpq/DV3949-700', xpath=xpath_nike)
+# p.parse_shoes(url='https://www.nike.com/t/free-metcon-5-mens-training-shoes-Vfsbpq/DV3949-700', xpath=xpath_nike)
+
+p.parse_shoes(url='https://www.nike.com/t/air-zoom-flight-95-mens-shoes-zc42bP/DX5505-100', xpath=xpath_nike)
 
 # /html/body/div[4]/div/div/div[2]/div/div[4]/div[2]/div[2]/div/div/div[3]/form/div[1]/fieldset/div
 # /html/body/div[4]/div/div/div[2]/div/div[4]/div[2]/div[2]/div/div/div[3]/form/div[1]/fieldset/div/div[1]
